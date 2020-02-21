1c855e440b4677d860892c62537b8728dd80c222
nandhas31 <nandhasundar@gmail.com>
2020-02-21 09:44:04 -0500

    Added Euclid.java

--- school/src/school/Euclid.java
+++ school/src/school/Euclid.java
@@ -2,7 +2,6 @@ package school;
 
 public class Euclid {
 	public static int gcd(int num1, int num2) {
-		//int quotient = num1/num2;
 		
 		if (num2 == 0)
 			return num1;
@@ -13,7 +12,6 @@ public class Euclid {
 		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(gcd(5,20));
 	}
 
